03f6dab085bfec8da5f6378cad4a9e93e0936cf7
Usman Tahir <hello@usmantahir.com>
2024-01-21 17:28:25 +0100

    Added MUI with a small custom theme

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#34495e",
+    },
+    secondary: {
+      main: "#9b59b6",
+    },
+    error: {
+      main: "#e74c3c",
+    },
+  },
+})
+
+export default theme
